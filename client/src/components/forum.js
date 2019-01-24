6ca16e152bd1dcf222374355a1321c170b5f0027
DrF <sp.knoll.86@gmail.com>
2019-01-23 18:40:55 -0600

    merge conflict

---
+++ client/src/components/forum.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Forum extends React.Component {
+    render () {
+        return (
+            <section id="forum" className="pb-0 forum">
+                <div>
+                    
+                </div>
+            </section>
+        )
+    }
+}
