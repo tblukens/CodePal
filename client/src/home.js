a1f601281981ab0b0a2aef221f04d4b39cba8639
mjbenefiel <michaeljbenefiel@gmail.com>
2019-01-26 12:24:36 -0600

    01262019 home.js cleanup

--- client/src/home.js
+++ client/src/home.js
@@ -5,7 +5,6 @@ import Footer from "./components/footer";
 import { Link } from "react-router-dom";
 import Auth from "./utility/Auth";
 
-
 class Home extends React.Component {
   componentDidMount() {
     const auth = new Auth();
@@ -46,11 +45,6 @@ class Home extends React.Component {
                     Potential Matches
                   </a>
                 </li>
-                {/* <li className="nav-item"> 
-                  <a className="nav-link" href="#feature">
-                    feature
-                  </a>
-                </li> */}
 
                 <li className="nav-item">
                   <a className="nav-link" href="#d">
@@ -58,7 +52,7 @@ class Home extends React.Component {
                   </a>
                 </li>
 
-                  <li className="nav-item">
+                <li className="nav-item">
                   <Link
                     className="nav-link"
                     to={`${process.env.PUBLIC_URL}/profiles`}
@@ -66,7 +60,7 @@ class Home extends React.Component {
                     Profile
                   </Link>
                 </li>
-               
+
                 <li className="nav-item">
                   <a className="nav-link" href="#contact">
                     Contact Us
@@ -81,7 +75,7 @@ class Home extends React.Component {
                     Sign In
                   </Link>
                 </li>
-               <li className="nav-item dropdown">
+                <li className="nav-item dropdown">
                   <a
                     className="nav-link dropdown-toggle"
                     data-toggle="dropdown"
@@ -167,7 +161,7 @@ class Home extends React.Component {
                     </li>
                   </ul>
                 </li>
-              </ul> 
+              </ul>
             </div>
           </div>
         </nav>
@@ -188,162 +182,21 @@ class Home extends React.Component {
                       <h1>
                         Connect with coders in your area, or across the globe.
                         <br />
-                     
                       </h1>
-                    
-                      {/* <div className="slid-btn"> 
-                        <a href="">
-                          <img
-                            src="assets/images/app1.png"
-                            alt="app1"
-                            className="img-fluid"
-                            data-tilt
-                            data-tilt-perspective="50"
-                            data-tilt-speed="350"
-                            data-tilt-max="1.8"
-                          />
-                        </a>
-                        <a href="">
-                          <img
-                            src="assets/images/app2.png"
-                            alt="app2"
-                            className="img-fluid"
-                            data-tilt
-                            data-tilt-perspective="50"
-                            data-tilt-speed="350"
-                            data-tilt-max="1.8"
-                          />
-                        </a>
-                      </div> */}
                     </div>
                   </div>
-                </div>
-              </div>
-    
-                  {/* <div className="profile-msg"> 
-                    <img
-                      src="assets/images/top2.png"
-                      alt="top2"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="awesome">
-                    <img
-                      src="assets/images/top3.png"
-                      alt="top3"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="profile-1">
-                    <img
-                      src="assets/images/top4.png"
-                      alt="top4"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="emoji">
-                    <img
-                      src="assets/images/top5.png"
-                      alt="top5"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="profile-2">
-                    <img
-                      src="assets/images/top1.png"
-                      alt="top1"
-                      className="img-fluid"
-                    />
-                  </div>*/}
-                </div>
-            </div> 
-             
-         
-        </section>
-        {/* Home One Section End */}
-
-        {/* About Component*/}
-        {/* <About /> */}
-
-        {/*ScreenShot Component*/}
-        <Team />
-
-        {/*Work Component
-        <Work /> */}
-
-        {/*Team Component
-        <Team />*/}
-
-        {/*Price Component
-        <Price /> */}
-
-        {/*Testimonial Component
-        <Testimonial /> */}
-
-        {/*Faq Component
-        <FAQ /> */}
-
-        {/*Blog Component
-        <Blog /> */}
-
-        {/*download section
-        <section className="download-bg">
-          <div className="container">
-            <div className="row">
-              <div className="col-xl-3 display-flex">
-                <div className="footer-logo">
-                  <img
-                    src="assets/images/footer-codepal-logo-white-sm.png"
-                    alt="footer-logo"
-                    className="img-fluid"
-                  />
-                </div>
-              </div>
-              <div className="col-xl-5 display-flex">
-                <div className="download-text">
-                  <h3>Download the CodePal app and enjoy it</h3>
-                </div>
-              </div>
-              <div className="col-xl-4 display-flex">
-                <div className="download-img">
-                  <ul>
-                    <li>
-                      <img
-                        src="assets/images/app1.png"
-                        alt="foot-app"
-                        className="img-fluid"
-                        data-tilt
-                        data-tilt-perspective="50"
-                        data-tilt-speed="350"
-                        data-tilt-max="1.8"
-                      />
-                    </li>
-                    <li>
-                      <img
-                        src="assets/images/app2.png"
-                        alt="foot-app1"
-                        className="img-fluid"
-                        data-tilt
-                        data-tilt-perspective="50"
-                        data-tilt-speed="350"
-                        data-tilt-max="1.8"
-                      />
-                    </li>
-                  </ul>
                 </div>
               </div>
             </div>
           </div>
         </section>
-        {/*end download section*/}
+
+        {/* Team Component */}
+        <Team />
 
         {/*Subscription Component*/}
         <Subscribe />
 
-        {/*Contact Component*/}
-        {/* <Contact /> */}
-
-        {/*Footer Component*/}
         <Footer />
 
         {/*tap-top*/}
