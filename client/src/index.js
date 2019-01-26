d8384c97a607e108d90052913c26dd0ba325e42a
Dustin Watkins <watkins656@gmail.com>
2019-01-26 13:22:13 -0600

    removed Dustin Willis again

--- client/src/index.js
+++ client/src/index.js
@@ -84,7 +84,7 @@ window.setState = (changes) => {
 //======================================
 //Authentication
 //======================================
-let username = auth.getProfile().given_name || "Dustin Willis";
+let username = auth.getProfile().given_name || "";
 
 let initialState = {
 	name: username,
