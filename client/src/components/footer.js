722f01870600284841ca265b35bb817131ba7091
mjbenefiel <michaeljbenefiel@gmail.com>
2019-01-28 17:13:11 -0600

    01282019 archived deleted files

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -8,7 +8,7 @@ class Footer extends React.Component {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+        // js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
         fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
     }
